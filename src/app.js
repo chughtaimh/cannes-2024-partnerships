4f9fea40c80f6c2cea20dca90d11e91d2590fc2f
Mohammad Chughtai <chughtai.mh@gmail.com>
2024-06-17 16:31:05 -0400

    Update app.js
    connect app.js to index.html

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
+// app.js
 import express from 'express';
+import path from 'path';
 import db from './database.js';
 import companiesRouter from './routes/companies.js';
 import partnershipsRouter from './routes/partnerships.js';
 
 const app = express();
 app.use(express.json());
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/company', companiesRouter);
 app.use('/partnership', partnershipsRouter);
