2ccf2700684146e10c89a6ab7a36aa5b6b9a6fc0
Mohammad Chughtai <chughtai.mh@gmail.com>
2024-06-17 13:32:19 -0400

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const db = require('./database');
+
+app.use(express.json());
+
+const companiesRouter = require('./routes/companies');
+const partnershipsRouter = require('./routes/partnerships');
+
+app.use('/company', companiesRouter);
+app.use('/partnership', partnershipsRouter);
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
