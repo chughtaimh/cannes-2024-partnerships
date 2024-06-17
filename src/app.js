409e6e4c4808642e0ea3c7b7b0f046577e0ae0d2
Mohammad Chughtai <chughtai.mh@gmail.com>
2024-06-17 16:50:58 -0400

    Connected app.js to index.html

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
-// app.js
 import express from 'express';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import db from './database.js';
 import companiesRouter from './routes/companies.js';
 import partnershipsRouter from './routes/partnerships.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 app.use(express.json());
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('./public'));
 
 app.use('/company', companiesRouter);
 app.use('/partnership', partnershipsRouter);
+
+app.get('/', (req, res) => {
+    res.sendFile('./public/index.html', { root: __dirname });
+});
 
 if (process.env.NODE_ENV !== 'test') {
     app.listen(3000, () => {
